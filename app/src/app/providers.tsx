3724758a7d0495b61418e441b8611f44e8e99006
bford21 <brianfordp@gmail.com>
2025-06-03 10:23:56 -0400

    lint errors

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -3,7 +3,7 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import { mainnet, base, baseSepolia } from 'wagmi/chains';
+import { mainnet, baseSepolia } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
